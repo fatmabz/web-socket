7c2f78e379ebbc24b9a1b106d665aa2b11ef99ab
fatma-bz1411 <fatma.bouzaabia@imbus.tn>
2018-04-02 11:37:57 +0100

    first commit

---
+++ src/app/services/hero/hero.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import {Http, Headers, RequestOptions, Response} from '@angular/http';
+import {Hero} from './hero';
+import 'rxjs/add/operator/toPromise';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/Rx';
+
+@Injectable()
+export class HeroService {
+  private heroesUrl = 'app/heroes';  //URL to JSON file
+  private headers = new Headers({'Content-Type': 'application/json'});
+
+  constructor (private http: Http) {}
+  getHeroes(): Promise<Hero[]> {
+    return this.http.get(this.heroesUrl)
+               .toPromise()
+               .then(response => response.json().data as Hero[])
+               .catch(this.handleError);
+  }
+
+ create(name: string): Promise<Hero> {
+    return this.http
+      .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+      .toPromise()
+      .then(res => res.json().data)
+      .catch(this.handleError);
+  }
+
+ delete(id: number): Promise<void> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete(url, {headers: this.headers})
+      .toPromise()
+      .then(() => null)
+      .catch(this.handleError);
+  }
+   update(hero: Hero): Promise<Hero> {
+    const url = `${this.heroesUrl}/${hero.id}`;
+    return this.http
+      .put(url, JSON.stringify(hero), {headers: this.headers})
+      .toPromise()
+      .then(() => hero)
+      .catch(this.handleError);
+  }
+  
+    private extractData(res: Response) {
+    let body = res.json();
+    return body.data || { };
+  }
+  
+  private handleError (error: Response | any) {
+    // In a real world app, we might use a remote logging infrastructure
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
+}
